0a8243edc4e78ff6295f8e64e9960d02fc1ad72a
Sushank Sakthivel Nagarajan <sushank82000@gmail.com>
2026-06-07 17:27:40 +0530

    chore: scaffold Spring Boot project with H2 and Maven

---
+++ src/main/java/com/refund/RefundApplication.java
@@ -0,0 +1,11 @@
+package com.refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RefundApplication.class, args);
+    }
+}
